9cc774834879d7482b61c4532a160019a7484ade
Abdulr <abdulrzk44@gmail.com>
2022-05-19 10:00:09 +0700

    update code

--- app/src/main/java/com/example/qiblafinder/MainActivity.java
+++ app/src/main/java/com/example/qiblafinder/MainActivity.java
@@ -7,20 +7,30 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.webkit.GeolocationPermissions;
-import android.webkit.PermissionRequest;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.GeolocationPermissions;
+import android.webkit.PermissionRequest;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,8 +38,11 @@ public class MainActivity extends AppCompatActivity {
 
     Context context;
     WebView myWebView;
-    private static final int MY_PERMISSIONS_REQUEST_CAMERA=1;
-    private static final int MY_PERMISSIONS_ACCESS_LOCATION=0;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private static final int MY_PERMISSIONS_ACCESS_LOCATION = 1;
+    private PermissionRequest mPermissionRequest;
+
+    private Camera camera;
 
     public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
 
@@ -40,26 +53,77 @@ public class MainActivity extends AppCompatActivity {
 
         myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         myWebView.getSettings().setDomStorageEnabled(true);
         myWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setAllowFileAccessFromFileURLs(true);
+        myWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
 
-        webSettings.setJavaScriptEnabled(true);
 
-        if(checkAndRequestPermissions()) {
+        if (checkAndRequestPermissions()) {
             // carry on the normal flow, as the case of  permissions  granted.
 
         }
 
         myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("https://qiblafinder.withgoogle.com/");
 
         myWebView.setWebChromeClient(new WebChromeClient() {
 
             @Override
             public void onPermissionRequest(final PermissionRequest request) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    request.grant(request.getResources());
+                mPermissionRequest = request;
+                final String[] requestedResources = request.getResources();
+                for (String r : requestedResources) {
+//                    if (r.equals(PermissionRequest.RESOURCE_VIDEO_CAPTURE)) {
+//                        // In this sample, we only accept video capture request.
+//                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this)
+//                                .setTitle("Allow Permission to camera")
+//                                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+//                                    @Override
+//                                    public void onClick(DialogInterface dialog, int which) {
+//                                        dialog.dismiss();
+//                                        mPermissionRequest.grant(new String[]{PermissionRequest.RESOURCE_VIDEO_CAPTURE});
+////                                        Log.d(TAG,"Granted");
+//                                    }
+//                                })
+//                                .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+//                                    @Override
+//                                    public void onClick(DialogInterface dialog, int which) {
+//                                        dialog.dismiss();
+//                                        mPermissionRequest.deny();
+////                                        Log.d(TAG,"Denied");
+//                                    }
+//                                });
+//                        AlertDialog alertDialog = alertDialogBuilder.create();
+//                        alertDialog.show();
+//
+//                        break;
+//                    }
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Allow Permission to "+r.toString())
+                            .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    mPermissionRequest.grant(new String[]{r});
+                                    dialog.dismiss();
+//                                        Log.d(TAG,"Granted");
+                                }
+                            })
+                            .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    mPermissionRequest.deny();
+//                                        Log.d(TAG,"Denied");
+                                }
+                            });
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
                 }
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//                    request.grant(request.getResources());
+//                }
             }
 
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
@@ -76,6 +140,50 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, permission_list, 1);
         }
 
+        try {
+            Method m = WebSettings.class.getMethod("setMixedContentMode", int.class);
+            if (m == null) {
+                Log.e("WebSettings", "Error getting setMixedContentMode method");
+            } else {
+                m.invoke(myWebView.getSettings(), 2); // 2 = MIXED_CONTENT_COMPATIBILITY_MODE
+                Log.i("WebSettings", "Successfully set MIXED_CONTENT_COMPATIBILITY_MODE");
+            }
+        } catch (Exception ex) {
+            Log.e("WebSettings", "Error calling setMixedContentMode: " + ex.getMessage(), ex);
+        }
+
+
+//        camera.getMatrix();
+
+//        camera.updateMatrix(); // make sure camera's local matrix is updated
+//        camera.updateMatrixWorld(); // make sure camera's world matrix is updated
+//        camera.matrixWorldInverse.getInverse( camera.matrixWorld );
+
+//        plane.updateMatrix(); // make sure plane's local matrix is updated
+//        plane.updateMatrixWorld(); // make sure plane's world matrix is updated
+
+//        var frustum = new THREE.Frustum();
+//        frustum.setFromMatrix( new THREE.Matrix4().multiplyMatrices( camera.projectionMatrix, camera.matrixWorldInverse ) );
+//        alert( frustum.contains( plane ) );
+
+
+        myWebView.loadUrl("https://qiblafinder.withgoogle.com/");
+
+//        Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+//        Uri intentUri =
+//                Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
+//                        .appendQueryParameter("file", "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf")
+//                        .appendQueryParameter("mode", "ar_preferred")
+//                        .build();
+//        sceneViewerIntent.setData(intentUri);
+//        sceneViewerIntent.setPackage("com.google.ar.core");
+//        startActivity(sceneViewerIntent);
+
+
+//        Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+//        sceneViewerIntent.setData(Uri.parse("https://arvr.google.com/scene-viewer/1.0?file=https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf"));
+//        sceneViewerIntent.setPackage("com.google.android.googlequicksearchbox");
+//        startActivity(sceneViewerIntent);
 
 //        WebView webView = (WebView) findViewById(R.id.webview);
 //        webView.getSettings().setLoadsImagesAutomatically(true);
@@ -100,17 +208,17 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 1) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(MainActivity.this,"permission granted", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "permission granted", Toast.LENGTH_SHORT).show();
                 // perform your action here
 
             } else {
-                Toast.makeText(MainActivity.this,"permission not granted", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "permission not granted", Toast.LENGTH_SHORT).show();
             }
         }
 
     }
 
-    private  boolean checkAndRequestPermissions() {
+    private boolean checkAndRequestPermissions() {
         int cameraPermission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.CAMERA);
         int locationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
@@ -122,7 +230,7 @@ public class MainActivity extends AppCompatActivity {
             listPermissionsNeeded.add(Manifest.permission.CAMERA);
         }
         if (!listPermissionsNeeded.isEmpty()) {
-            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
             return false;
         }
         return true;
